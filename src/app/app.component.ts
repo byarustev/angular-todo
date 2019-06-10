089ae139e7280b382b4063039363a5c3aecc728f
byarustev <byarus45@gmail.com>
2019-06-10 11:49:33 +0300

    add changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'angular2';
+  title: string;
+  todoList: string[];
+  constructor() {
+    this.title = 'My todo App';
+    this.todoList = ['practice angular', 'UI/Ux practice', 'React Native'];
+  }
+
+  deleteTodo(i) {
+    this.todoList.splice(i, 1);
+  }
+
+  saveTodo(todo) {
+    if(todo!=="" ){
+      this.todoList.push(todo);
+    }
+
+  }
 }
